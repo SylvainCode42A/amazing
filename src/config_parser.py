e58f5edd3cf1db0117bbad406fa23e27e9ec033d
DayraN19 <lixxxuuusss@gmail.com>
2026-04-03 17:17:39 +0200

    conf

--- src/config_parser.py
+++ src/config_parser.py
@@ -1,12 +1,25 @@
 import os
 
 
-def config_parser(filename: str):
+def config_parser(filename: str) -> None:
 
-    required = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"]
+    k_required = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"]
 
     if not os.path.exists(filename):
-        raise FileNotFoundError(f"Fichier config introuvable : {filename}")
-    
-    with open(filename, "r") as file:
-        file.read("WIDTH")
+        raise FileNotFoundError(f"Fichier introuvable : {filename}")
+
+    with open(filename, "r") as f:
+        line = f.read()
+        for key in k_required:
+            if key not in line:
+                raise KeyError(f"Error {key} key not found in {filename}")
+        else:
+            print("All good")
+
+
+def main() -> None:
+    config_parser("config.txt")
+
+
+if __name__ == "__main__":
+    main()
