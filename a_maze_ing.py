4a71d6699291504cc6262e1976e853aad785f25d
Lidrissi Sylvain <sylvain@MacBook-Air-de-Lidrissi.local>
2026-04-10 18:04:54 +0200

    11/10

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -10,45 +10,128 @@ N, E, S, W = 1, 2, 4, 8
 
 
 def main() -> None:
-    dict = parse_config("config.txt")
-
-    if not verify_dict(dict):
+    try:
+        dict = parse_config("config.txt")
+    except Exception as e:
+        print(e)
         return
+
+    try:
+        verify_dict(dict)
+    except Exception as e:
+        print(e)
+        return
+
+    width = int(dict["WIDTH"])
+    height = int(dict["HEIGHT"])
+
+    grid, forbidden = create_grid(width, height)
+
+    value = dict["ENTRY"]
+    x_, y_ = value.split(",")
+    x_start, y_start = int(x_), int(y_)
+    open_cell(
+        grid,
+        x_start,
+        y_start,
+        width,
+        height
+        )
+
+    if "SEED" in dict:
+        seed = int(dict["SEED"])
     else:
-        width = int(dict["WIDTH"])
-        height = int(dict["HEIGHT"])
+        seed = None
 
-        grid, forbidden = create_grid(width, height)
+    exit = dict["EXIT"]
+    x_, y_ = exit.split(",")
+    x_exit, y_exit = int(x_), int(y_)
 
-        value = dict["ENTRY"]
-        x_, y_ = value.split(",")
-        x_start, y_start = int(x_), int(y_)
-        open_cell(
+    open_cell(
+        grid,
+        x_exit,
+        y_exit,
+        width,
+        height
+        )
+
+    grid = generate(
+                grid,
+                width,
+                height,
+                x_start,
+                y_start,
+                forbidden,
+                seed
+                )
+
+    if dict["PERFECT"] == "False":
+        grid = make_imperfect(
             grid,
-            x_start,
-            y_start,
             width,
-            height
+            height,
+            seed
             )
 
-        if "SEED" in dict:
-            seed = int(dict["SEED"])
-        else:
-            seed = None
+    path, direction = find_exit(
+        grid,
+        width,
+        height,
+        (x_start, y_start),
+        (x_exit, y_exit)
+        )
 
-        exit = dict["EXIT"]
-        x_, y_ = exit.split(",")
-        x_exit, y_exit = int(x_), int(y_)
+    write_maze(
+        grid,
+        (x_start, y_start),
+        (x_exit, y_exit),
+        direction,
+        dict["OUTPUT_FILE"]
+        )
 
-        open_cell(
-            grid,
-            x_exit,
-            y_exit,
-            width,
-            height
-            )
+    show_path = False
+    color = 0
 
-        grid = generate(
+    try:
+        while True:
+            print("\033[H\033[J", end="")
+            display_maze(
+                    grid,
+                    (x_start, y_start),
+                    (x_exit, y_exit),
+                    path if show_path else [],
+                    forbidden,
+                    color
+                    )
+
+            print("\np = show/hide path | r = regenerate |"
+                  " c = change color | q = quit")
+
+            key = readchar.readkey()
+
+            if key == 'q':
+                return
+
+            elif key == 'p':
+                show_path = not show_path
+
+            elif key == 'r':
+
+                grid, forbidden = create_grid(width, height)
+                open_cell(
+                    grid,
+                    x_start,
+                    y_start,
+                    width,
+                    height
+                    )
+
+                if "SEED" in dict:
+                    seed = int(dict["SEED"])
+                else:
+                    seed = None
+
+                grid = generate(
                     grid,
                     width,
                     height,
@@ -58,121 +141,46 @@ def main() -> None:
                     seed
                     )
 
-        if dict["PERFECT"] == "False":
-            grid = make_imperfect(
-                grid,
-                width,
-                height,
-                seed
-                )
+                open_cell(
+                    grid,
+                    x_exit,
+                    y_exit,
+                    width,
+                    height
+                    )
 
-        path, direction = find_exit(
-            grid,
-            width,
-            height,
-            (x_start, y_start),
-            (x_exit, y_exit)
-            )
-
-        write_maze(
-            grid,
-            (x_start, y_start),
-            (x_exit, y_exit),
-            direction,
-            dict["OUTPUT_FILE"]
-            )
-
-        show_path = False
-        color = 0
-
-        try:
-            while True:
-                print("\033[H\033[J", end="")
-                display_maze(
-                        grid,
-                        (x_start, y_start),
-                        (x_exit, y_exit),
-                        path if show_path else [],
-                        forbidden,
-                        color
-                        )
-
-                print("\np = show/hide path | r = regenerate |"
-                      " c = change color | q = quit")
-
-                key = readchar.readkey()
-
-                if key == 'q':
-                    return
-
-                elif key == 'p':
-                    show_path = not show_path
-
-                elif key == 'r':
-
-                    grid, forbidden = create_grid(width, height)
-                    open_cell(
-                        grid,
-                        x_start,
-                        y_start,
-                        width,
-                        height
-                        )
-
-                    if "SEED" in dict:
-                        seed = int(dict["SEED"])
-                    else:
-                        seed = None
-
-                    grid = generate(
+                if dict["PERFECT"] == "False":
+                    grid = make_imperfect(
                         grid,
                         width,
                         height,
-                        x_start,
-                        y_start,
-                        forbidden,
                         seed
                         )
 
-                    open_cell(
-                        grid,
-                        x_exit,
-                        y_exit,
-                        width,
-                        height
-                        )
+                path, direction = find_exit(
+                    grid,
+                    width,
+                    height,
+                    (x_start, y_start),
+                    (x_exit, y_exit)
+                    )
 
-                    if dict["PERFECT"] == "False":
-                        grid = make_imperfect(grid,
-                                              width,
-                                              height,
-                                              seed
-                                              )
+                write_maze(
+                    grid,
+                    (x_start, y_start),
+                    (x_exit, y_exit),
+                    direction,
+                    dict["OUTPUT_FILE"]
+                    )
 
-                    path, direction = find_exit(
-                        grid,
-                        width,
-                        height,
-                        (x_start, y_start),
-                        (x_exit, y_exit)
-                        )
+            elif key == 'c':
+                if color + 1 < 5:
+                    color += 1
+                else:
+                    color = 0
 
-                    write_maze(
-                        grid,
-                        (x_start, y_start),
-                        (x_exit, y_exit),
-                        direction,
-                        dict["OUTPUT_FILE"]
-                        )
-
-                elif key == 'c':
-                    if color + 1 < 5:
-                        color += 1
-                    else:
-                        color = 0
-
-        except KeyboardInterrupt:
-            return
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == "__main__":
--- src/config_parser.py
+++ src/config_parser.py
@@ -2,12 +2,16 @@ def parse_config(filename: str) -> dict:
 
     dict_file = {}
 
-    with open(filename, "r") as f:
-        for line in f:
-            if line.strip() == "":
-                continue
-            key, value = line.split("=")
-            dict_file[key] = value.strip()
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                if line.strip() == "":
+                    continue
+                key, value = line.split("=")
+                dict_file[key] = value.strip()
+    except FileNotFoundError:
+        raise FileNotFoundError("Sorry, config.txt not found")
+
     return dict_file
 
 
@@ -16,8 +20,9 @@ def verify_dict(dict_file: dict) -> bool:
     required = ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"]
 
     for word in required:
-        if word not in dict_file.keys():
-            return False
-        if not dict_file[word]:
-            return False
+        if word not in dict_file:
+            raise ValueError(f"Missing required attribute: '{word}'")
+        if not dict_file[word].strip():
+            raise ValueError(f"Attribute '{word}' is empty.")
+
     return True
